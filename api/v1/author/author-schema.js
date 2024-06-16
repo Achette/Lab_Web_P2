b1b01dbb2966ae7c216b2e79ad85ac4a55a53130
Achette <i.achette@live.com>
2024-06-16 15:03:59 -0300

    feat: cria schema para author

---
+++ api/v1/author/author-schema.js
@@ -0,0 +1,27 @@
+const Joi = require("joi");
+
+const createSchema = {
+  payload: Joi.object({
+    name: Joi.string().required(),
+    biography: Joi.string().required(),
+    birthDate: Joi.string().required(),
+  }),
+};
+
+const getById = {
+  params: Joi.object({
+    id: Joi.string().required(),
+  }),
+};
+
+const deleteById = {
+  params: Joi.object({
+    id: Joi.string().required(),
+  }),
+};
+
+module.exports = {
+  createSchema,
+  getById,
+  deleteById,
+};
